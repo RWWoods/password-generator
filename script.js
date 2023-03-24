bf038e019f1766f8336e7ce4d847be4751b55094
Kiggles <ridgewwoods@gmail.com>
2023-03-24 18:13:31 -0400

    finished code, works as intended. need to add comments and readme

--- script.js
+++ script.js
@@ -14,10 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 var passLength = prompt("Choose a password length between 8 and 128 characters")
-var capitalPrompt = prompt("Do you want to use capital letters?");
-var lowerPrompt = prompt("Do you want to use lowercase letters?")
-var charPrompt = prompt("Do you want to use special characters?")
-var numPrompt = prompt("Do you want to use numbers?")
+if (passLength < 8 || passLength > 128) {
+    prompt("You must choose a number between 8 & 128");
+} else {
+    var capitalPrompt = prompt("Do you want to use capital letters?");
+    var lowerPrompt = prompt("Do you want to use lowercase letters?")
+    var charPrompt = prompt("Do you want to use special characters?")
+    var numPrompt = prompt("Do you want to use numbers?")
+    if (capitalPrompt == null && lowerPrompt == null && charPrompt == null && numPrompt == null) {
+        prompt("you must choose at least one option!");
+    }
+}
 
 var capitalArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -42,21 +49,17 @@ for (let i = 0; i < choices.length; i++) {
 
 
 function generatePassword() {
-    if (passLength < 8 || passLength > 128) {
-        prompt("You must choose a number between 8 & 128");
-    }
-    else if (capitalPrompt == null && lowerPrompt == null && charPrompt == null && numPrompt == null) {
-        prompt("you must choose at least one option!");
-    }
-    else {
-        newArray = passArray.flat();
+    newArray = passArray.flat();
 
-        for (let x = 0; x < passLength; x++) {
-            rand = Math.floor(Math.random() * newArray.length);
-            passFinal.push(newArray[rand]);
-        }
+    for (let x = 0; x < passLength; x++) {
+        rand = Math.floor(Math.random() * newArray.length);
+        passFinal.push(newArray[rand]);
     }
-    return passFinal;
+    return passFinal.join("");
 }
 
+
 generatePassword();
+
+
+
